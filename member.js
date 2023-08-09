6199519067646710c772a3687d1e23cd60d83a23
winardi <winardikwee@yahoo.com>
2023-08-09 14:41:18 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillMember() {
+    var member = {
+        name: 'John Doe',
+        age: 30,
+        address: {
+            street: '40 Main st',
+            city: 'Boston'
+        },
+        children: ['Brianna', 'Nicholas']
+    };
+    return member;
+}
